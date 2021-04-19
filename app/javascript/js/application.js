06557f66fa379dced6a7c514dc743768b3052c2c
marumaru1019 <ryu.ryusei401@gmail.com>
2021-04-19 20:44:39 +0900

    slight change

--- app/javascript/js/application.js
+++ app/javascript/js/application.js
@@ -4,7 +4,6 @@ import './import/google-map'
 
 //setting
 import './component/slick_set'
-import './component/login-check'
 import './component/registration-check'
 import './component/notice-check'
 import './component/profile-modal'
