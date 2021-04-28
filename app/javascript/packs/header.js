285f6ecd8cc5ad732ed79623441525b13daa7dc3
marumaru1019 <ryu.ryusei401@gmail.com>
2021-04-29 04:47:11 +0900

    CA-64: create what is modern & qa,, customize slick, slight responsive

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -4,4 +4,8 @@ document.addEventListener("turbolinks:load", function () {
         $("#js-ham").toggleClass('is-active');
         $("#js-modal").toggleClass("open");
     });
+    $("#js-ham-xs").click(function () {
+        $("#js-ham-xs").toggleClass('is-active');
+        $("#js-modal").toggleClass("open");
+    });
 });
