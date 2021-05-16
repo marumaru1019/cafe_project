54102c6eeedff36879237de9fefbd4e5ac7a6d8b
marumaru1019 <ryu.ryusei401@gmail.com>
2021-05-17 02:50:05 +0900

    CA-79: add quote colmun in event table

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,40 +1,8 @@
 ActiveAdmin.register Event do
-  permit_params :name, :date, :time_id, :recommend_menu, :recommend_menu_price, :place,:adress, :max_num, :comment, :store_url, :image
-
-  form do |f|
-    f.semantic_errors # shows errors on :base
-    f.inputs do
-      f.input :name
-      f.input :date
-      f.input :time_id
-      f.input :recommend_menu
-      f.input :recommend_menu_price
-      f.input :place
-      f.input :adress
-      f.input :max_num
-      f.input :image, as: :file
-      f.input :comment
-      f.input :store_url
-      f.actions
-    end
-  end
-
-  show do
-    attributes_table do
-      row :name
-      row :date
-      row :time_id
-      row :recommend_menu
-      row :recommend_menu_price
-      row :place
-      row :adress
-      row :max_num
-      row :comment
-      row :store_url
-      row :image do |ad|
-        image_tag url_for(ad.image)
-      end
-      active_admin_comments
+  # 全てのカラムの操作を許可
+  controller do
+    def permitted_params
+      params.permit!
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_171818) do
+ActiveRecord::Schema.define(version: 2021_05_16_112922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_04_05_171818) do
     t.integer "recommend_menu_price"
     t.date "date"
     t.text "adress"
+    t.string "quote"
   end
 
   create_table "hellos", force: :cascade do |t|
